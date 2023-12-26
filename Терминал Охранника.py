f338a777bc008da3f14024b8d0987c7909bc5b49
Ivvang <154535693+Ivvang@users.noreply.github.com>
2023-12-26 19:11:32 +0300

    Add files via upload

--- Терминал Охранника.py
+++ Терминал Охранника.py
@@ -2,39 +2,11 @@ import sys
 import sqlite3 as sq 
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout , QLineEdit,QRadioButton
 import time
-
+from base1 import base1
 sotryd=25
 gosti=5
 
 
-
-base1=[("Алексеев","Илья","Алексеевич","23-Apr-1994","директор по информатизации","+79993456785","aleksee45@mail.ru","NULL","NULL"),
-("Аржанов","Владислав","Александрович","27-Jan-1995","начальние отдела кадров","+79063194556","arhjanov12@yandex.ru","NULL","NULL"),
-("Белоцерковец ","Дмитрий","Александрович","12-Sep-1983","менеджер","+79773332123","belotserkovets@gmail.com","NULL","NULL"),
-("Богушев","Арсений","Александрович","09-Dec-1973","директор","+79264444334","Bogushev@mail.ru","NULL","NULL"),
-("Гундарова","Софья","Анатольевна","30-Nov-1995","менеджер","+79256789012","Gundarova@yandex.ru","NULL","NULL"),
-("Долженков","Борис","Алексеевич","23-Jun-2002","курьер","+79003196748","Dolzhenkov@gmail.com","NULL","NULL"),
-("Журавова","Ангелина","Сергеевна","17-Aug-2002","специалист по корреспонденции","+79836571245","Zhuravova@gmail.com","NULL","NULL"),
-("Клевцова","Варвара","Андреевна","15-Oct-2002","секретарь","+79023672156","Klevtsova@gmail.com","NULL","NULL"),
-("Коваленко","Вероника","Александровна","17-Jul-1997","менеджер","+79271285522","Kovalenko@gmail.com","NULL","NULL"),
-("Королев","Платон","Валеорьевич","13-Mar-1993","главный бухгалтер","+79773045689","Korolev@gmail.com","NULL","NULL"),
-("Кубышкин","Роман","Евгеньевич","10-Oct-1985","менеджер по технологии","+79013219045","Kubyshkin@gmail.com","NULL","NULL"),
-("Макаренко","Константин","Викторович","11-Nov-1984","бизнес аналитик","+79992341289","Makarenko@gmail.com","NULL","NULL"),
-("Мартиросов","Артемий","Артурович","01-Feb-1974","smm - менеджер","+79027893459","Martirosov@gmail.com","NULL","NULL"),
-("Мелкумян","Роберт","Робертович","16-May-1993","маркетолог","+79982341290","Melkumyan@gmail.com","NULL","NULL"),
-("Олейник","Ольга","Викторовна","18-Jun-1993","руководитель проекта","+79762349987","Oleinik@gmail.com","NULL","NULL"),
-("Полюцкий","Александр","Сергеевич","19-Jan-1981","юрист-консультант","+79673421894","Polyutsky@gmail.com","NULL","NULL"),
-("Рустамов","Иброхим","Рустамович","29-Oct-1982","зав.хозяйством","+79231782392","Rustamov@gmail.com","NULL","NULL"),
-("Рябцева","Алена","Александровна","14-Nov-1982","бухгалтер","+79321743981","Ryabtseva@gmail.com","NULL","NULL"),
-("Саберова","Динара","Равилевна","28-Dec-1994","главный юрист","+79992341084","Saberova@gmail.com","NULL","NULL"),
-("Сафонова","Ирина","Ивановна","15-Jul-1983","делопроизводитель","+79253419861","Safonov@gmail.com","NULL","NULL"),
-("Стронина","София","Евгеньевна","31-Mar-2002","секретарь","+79162381629","Stronina@gmail.com","NULL","NULL"),
-("Тарасов","Степан","Павлович","06-Jun-1996","менеджер","+79152371823","Тарасов@gmail.com","NULL","NULL"),
-("Чекменёв","Кирилл","Андреевич","02-Aug-1998","дизайнер","+79172361959","Chekmenev@gmail.com","NULL","NULL"),
-("Чернов","Максим","Дмитриевич","23-Dec-1999","аналитик","+79293652732","Chernov@gmail.com","NULL","NULL"),
-("Шарофидинова","Ирода","Отабек","21-Mar-1991","уборщица","+79002893276","Sharofidinov@gmail.com","NULL","NULL"),
-("Шундрик","Егор","Максимович","07-Apr-1992","расчетчик","+79281123119","Shundrick@gmail.com","NULL","NULL"),
-("Шушняев","Никита","Максимовна","09-Sep-1991","сметчик","+79031732861","Shushnyaev@gmail.com","NULL","NULL")]
 
 
 local = time.ctime(time.time())
@@ -91,8 +63,8 @@ class PassControlApp(QWidget):
         self.RadioButton=QRadioButton("Наличие машины")
 
         self.issue_pass_button2 = QPushButton('Добавить временный пропуск')
-        self.issue_pass_button = QPushButton('Вошёл')
-        self.issue_pass_button3 = QPushButton('Вышел')
+        self.issue_pass_button = QPushButton('Вход')
+        self.issue_pass_button3 = QPushButton('Выход')
       
         self.issue_pass_button.clicked.connect(self.eks)
         self.issue_pass_button3.clicked.connect(self.exit)
@@ -109,6 +81,8 @@ class PassControlApp(QWidget):
 
         self.fath = QLineEdit(self,placeholderText="Введите отчество...")
 
+        self.num = QLineEdit(self,placeholderText="Введите номер телефона...")
+
         
 
         layout = QVBoxLayout()
@@ -119,6 +93,7 @@ class PassControlApp(QWidget):
         layout.addWidget(self.passv)
         layout.addWidget(self.fam)
         layout.addWidget(self.fath)
+        layout.addWidget(self.num)
         layout.addWidget(self.RadioButton)
         layout.addWidget(self.issue_pass_button2)
         layout.addWidget(self.issue_pass_button)
@@ -238,7 +213,7 @@ class App(QWidget):
 
         self.issue_pass_button2 = QPushButton('Выдать пропуск на выход')
         self.issue_pass_button = QPushButton('Выдать пропуск')
-        self.RadioButton=QRadioButton("Есть ли машина?")
+        self.RadioButton=QRadioButton("Наличие машины")
 
 
         self.issue_pass_button.clicked.connect(self.issue_pass)
@@ -246,16 +221,16 @@ class App(QWidget):
         self.RadioButton.toggled.connect(self.update)
         
       
-        self.exit= QLineEdit(self,placeholderText="Ведите контактную почту для выхода...")
+        self.exit= QLineEdit(self,placeholderText="Введите контактную почту для выхода...")
         
 
-        self.famil= QLineEdit(self,placeholderText="Ведите фамилию ...")
-        self.name= QLineEdit(self,placeholderText="Ведите имя...")
-        self.otch = QLineEdit(self,placeholderText="Ведите отчество...")
-        self.date= QLineEdit(self,placeholderText="Ведите дату рождения...")
-        self.cel= QLineEdit(self,placeholderText="Ведите цель визита...")
-        self.nomer = QLineEdit(self,placeholderText="Ведите номер телефона...")
-        self.pochta = QLineEdit(self,placeholderText="Ведите контактную почту...")
+        self.famil= QLineEdit(self,placeholderText="Введите фамилию ...")
+        self.name= QLineEdit(self,placeholderText="Введите имя...")
+        self.otch = QLineEdit(self,placeholderText="Введите отчество...")
+        self.date= QLineEdit(self,placeholderText="Введите дату рождения...")
+        self.cel= QLineEdit(self,placeholderText="Введите цель визита...")
+        self.nomer = QLineEdit(self,placeholderText="Введите номер телефона...")
+        self.pochta = QLineEdit(self,placeholderText="Введите контактную почту...")
 
         layout = QVBoxLayout()
 
@@ -285,7 +260,7 @@ class App(QWidget):
               self.car = 0
          
          if gosti ==0:
-                print("Порковочных мест нет приходите позже!!!")    
+                print("Парковочных мест нет! Приезжайте позже!")    
     
     def vremPROP(self):
        global gosti
@@ -322,7 +297,7 @@ class App(QWidget):
         global sotryd
         if self.car == 1:
              if gosti ==0:
-                  print("Парковочных мест нет приходите позже!!!")
+                  print("Парковочных мест нет! Приезжайте позже!")
              else:
                     sotryd=sotryd-1
                     gosti=gosti-1
